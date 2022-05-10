91400c2b6b1ea44508eea914bfdd4b0ac81baf13
maor@zeekit.me <M1g2u3n4a>
2022-05-10 23:48:45 +0300

    fix hover on mobile and replace it with active. remove the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Title} from './components/Title';
 import {GameBoard} from './components/Gameboard';
 
 const App = () => {
@@ -7,7 +6,6 @@ const App = () => {
     return (
         <div className='flex flex-col items-center justify-center pb-10 h-screen w-screen bg-slate-800'>
             <h2 className='font-display text-2xl text-white tracking-widest text-center mb-10 absolute top-2 left-2'>Best Score: {bestScore}</h2>
-            <Title title='simon' />
             <div className='w-80 h-80 px-3 bg-black rounded-full relative overflow-hidden md:w-96 md:h-96'>
                 <GameBoard setBestScore={setBestScore} />
             </div>
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -30,7 +30,7 @@ const GameBoard = ({setBestScore}:GameBoardProps) => {
         const lastColor = copyColors.pop();
 
         if (lastColor === name) {
-            SOUNDS[name].play();
+            await SOUNDS[name].play();
             if(copyColors.length){
                 setUserColors(copyColors);
             }else {
@@ -57,7 +57,6 @@ const GameBoard = ({setBestScore}:GameBoardProps) => {
     useEffect(() => {
         (async () => {
             for(let [index, color] of computerColors.entries()) {
-                SOUNDS[color].playbackRate = 1.5;
                 await SOUNDS[color].play();
                 setActiveColor(color);
                 await timeout(400);
@@ -74,10 +73,10 @@ const GameBoard = ({setBestScore}:GameBoardProps) => {
 
     return (
         <>
-            <button disabled={!gameStarted || computerTurn } name='green' onClick={handleUserClick} className={`${activeColor === 'green' ? 'bg-green-700' : 'bg-green-500'} absolute w-3/6 h-3/6 -top-1.5 -left-1.5 hover:bg-green-600`} />
-            <button disabled={!gameStarted || computerTurn } name='red' onClick={handleUserClick} className={`${activeColor === 'red' ? 'bg-red-700' : 'bg-red-500'} absolute w-3/6 h-3/6 -top-1.5 -right-1.5 hover:bg-red-600`} />
-            <button disabled={!gameStarted || computerTurn } name='yellow' onClick={handleUserClick} className={`${activeColor === 'yellow' ? 'bg-yellow-700' : 'bg-yellow-500'} absolute w-3/6 h-3/6 -bottom-1.5 -left-1.5 hover:bg-yellow-600`} />
-            <button disabled={!gameStarted || computerTurn } name='blue' onClick={handleUserClick} className={`${activeColor === 'blue' ? 'bg-blue-700' : 'bg-blue-500'} absolute w-3/6 h-3/6 -bottom-1.5 -right-1.5 hover:bg-blue-600`} />
+            <button disabled={!gameStarted || computerTurn } name='green' onMouseDown={handleUserClick} className={`${activeColor === 'green' ? 'bg-green-700' : 'bg-green-500'} absolute w-3/6 h-3/6 -top-1.5 -left-1.5 active:bg-green-600 md:hover:bg-green-600`} />
+            <button disabled={!gameStarted || computerTurn } name='red' onMouseDown={handleUserClick} className={`${activeColor === 'red' ? 'bg-red-700' : 'bg-red-500'} absolute w-3/6 h-3/6 -top-1.5 -right-1.5 active:bg-red-600 md:hover:bg-red-600`} />
+            <button disabled={!gameStarted || computerTurn } name='yellow' onMouseDown={handleUserClick} className={`${activeColor === 'yellow' ? 'bg-yellow-700' : 'bg-yellow-500'} absolute w-3/6 h-3/6 -bottom-1.5 -left-1.5 active:bg-yellow-600 md:hover:bg-yellow-600`} />
+            <button disabled={!gameStarted || computerTurn } name='blue' onMouseDown={handleUserClick} className={`${activeColor === 'blue' ? 'bg-blue-700' : 'bg-blue-500'} absolute w-3/6 h-3/6 -bottom-1.5 -right-1.5 active:bg-blue-600 md:hover:bg-blue-600`} />
             <div className='absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 w-36 h-36 bg-black rounded-full flex justify-center items-center'>
                 {!gameStarted && <Button mode={'start'} onClick={startHandle}/>}
             </div>
